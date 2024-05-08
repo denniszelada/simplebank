44877c4b7590d4d9f6f0f9310ffad3f781d61567
Dennis Zelada <denniszelada@gmail.com>
2024-05-08 14:55:26 +0200

    Update main_test function to return the testDB object

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -12,15 +12,18 @@ const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
+
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
