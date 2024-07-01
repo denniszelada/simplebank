2fadd7caf5916c3ba091c37f5185f8e45ae1770a
Dennis Zelada <denniszelada@gmail.com>
2024-07-01 17:15:45 +0200

    Add new table for users and relationship with accounts

---
+++ db/migration/000002_add_users.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINTS IF EXISTS "owner_currency_key";
+
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINTS IF EXISTS "accounts_owner_fkey";
+
+DROP TABLE IF EXISTS "users";
