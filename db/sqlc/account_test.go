0c4583ddaac3a8970312f0579b3f633a3bd7d5a3
Dennis Zelada <denniszelada@gmail.com>
2024-10-07 21:21:46 +0200

    Add the auth middleware to the account and transfer

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -54,8 +54,8 @@ func TestUpdateAccount(t *testing.T) {
 	account1 := createRandomAccount(t)
 
 	arg := UpdateAccountParams{
-		ID: 		account1.ID,
-		Balance: 	util.RandomMoney(),
+		ID:      account1.ID,
+		Balance: util.RandomMoney(),
 	}
 
 	account2, err := testQueries.UpdateAccount(context.Background(), arg)
@@ -81,20 +81,23 @@ func TestDeleteAccount(t *testing.T) {
 }
 
 func TestListAccounts(t *testing.T) {
-	for i:=0; i<10; i++ {
-		createRandomAccount(t)
+	var lastAccount Account
+	for i := 0; i < 10; i++ {
+		lastAccount = createRandomAccount(t)
 	}
 
-	arg := ListAccountsParams {
-		Limit: 5,
-		Offset: 5,
+	arg := ListAccountsParams{
+		Owner:  lastAccount.Owner,
+		Limit:  5,
+		Offset: 0,
 	}
 
 	accounts, err := testQueries.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
-	require.Len(t, accounts, 5)
+	require.NotEmpty(t, accounts)
 
-	for _, account := range accounts{
+	for _, account := range accounts {
 		require.NotEmpty(t, account)
+		require.Equal(t, lastAccount.Owner, account.Owner)
 	}
 }
